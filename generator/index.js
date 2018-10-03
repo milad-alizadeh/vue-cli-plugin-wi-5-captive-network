aac09b5539bfa5339f1d85bc742fd625ad43ab1c
Milad Alizadeh <milaadd@gmail.com>
2018-10-03 11:23:25 +0100

    update index.js

--- generator/index.js
+++ generator/index.js
@@ -4,8 +4,7 @@ module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
       "platform": "^1.3.5",
-      "axios": "^0.18.0",
-      "vue-tel-input": "^2.0.5",
+      "axios": "^0.18.0"
     }
   })
 
