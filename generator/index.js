1edd969c6b7dc1fd803aa1bad84a4461dfc28b29
Milad Alizadeh <milaadd@gmail.com>
2018-10-03 11:31:35 +0100

    remove dependencies

--- generator/index.js
+++ generator/index.js
@@ -1,12 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 
 module.exports = (api, options, rootOptions) => {
-  api.extendPackage({
-    dependencies: {
-      "platform": "^1.3.5",
-      "axios": "^0.18.0"
-    }
-  })
-
   api.render('./template')
 }
