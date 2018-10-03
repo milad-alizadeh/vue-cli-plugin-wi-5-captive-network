abd4e6b9a45ae024e83a0b587e6ce3e4013cf802
Milad Alizadeh <milaadd@gmail.com>
2018-10-03 12:01:58 +0100

    update generator

--- generator/index.js
+++ generator/index.js
@@ -3,10 +3,35 @@
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
-      "platform": "^1.3.5",
-      "axios": "^0.18.0"
+      'platform': '^1.3.5',
+      'axios': '^0.18.0'
+    },
+    vue: {
+      chainWebpack: (config) => {
+        config.plugins.delete('prefetch')
+      }
     }
   })
 
   api.render('./template')
+
+  api.postProcessFiles(files => {
+    if (main) {
+      files['src/main.js'] = `import bootstrapper from './bootstrapper'
+import { isCna } from '@/helpers'
+
+(async () => {
+  try {
+    await bootstrapper.hasInternet()
+    if (isCna()) {
+      import('./cna/main')
+    } else {
+      import('./web-app')
+    }
+  } catch (e) {
+    import('./cna/main')
+  }
+})()`
+    }
+  })
 }
